ceb88ff4baaf21738bb64743eb55002f684c3df8
lokeshkonjeti <47377961+lokeshkonjeti@users.noreply.github.com>
2019-03-10 10:31:09 +0530

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -50,3 +50,27 @@ Y_pred = classifier.predict(X_test)
 from sklearn import metrics
 print(metrics.accuracy_score(Y_test, Y_pred))
 
+#precision recall curve
+from sklearn.metrics import precision_recall_curve
+import matplotlib.pyplot as plt
+from sklearn.utils.fixes import signature
+
+precision, recall, _ = precision_recall_curve(Y_test, Y_pred)
+
+# In matplotlib < 1.5, plt.fill_between does not have a 'step' argument
+step_kwargs = ({'step': 'post'}
+               if 'step' in signature(plt.fill_between).parameters
+               else {})
+plt.step(recall, precision, color='b', alpha=0.2,
+         where='post')
+plt.fill_between(recall, precision, alpha=0.2, color='b', **step_kwargs)
+
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.ylim([0.0, 1.05])
+plt.xlim([0.0, 1.0])
+plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(
+          average_precision))
+
+
+
